3fe740d5fa81a14ed9653c1d8be7b5d3ae646440
Kevin Williams <KingArthur5o3x@gmx.com>
2024-11-09 23:13:40 -0600

    fix deployment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-export default function() {
+export default function Header() {
     return(
         <div className="flex items-center justify-between w-full p-4 bg-cppBlue ">
            <img src="/PPPPLogo.png" alt="logo" className="w-28 h-28 relative"></img> 
